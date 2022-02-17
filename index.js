359e3d822928e496586efdd3cd557747d58c2193
sllujaan <sllujaan44@gmail.com>
2022-02-17 09:46:24 +0500

    fixed

--- index.js
+++ index.js
@@ -1,66 +1,41 @@
 
 
-
-// Write a single function that takes two string inputs and returns a single string of either "TRUE"
-// or "FALSE". The function should determine if the first input is equal to the second input when
-// you are able to modify the first string input. The following modifications to the first string input
-// are permitted: letters may be capitalized, or discarded, but otherwise not changed.
-
-// Console.log the following examples:
-// ('ebCde','BCD') TRUE
-// ('eCB','ECB') TRUE (e was uppercased)
-// ('BcdEF','BCEF') TRUE (c was uppercased, d was discarded)
-// ('BCD','BcD') FALSE (C could not be lowercased)
-
-
+/**
+ * This function takes two string inputs and returns a single string of either "TRUE"
+ * or "FALSE". The function should determine if the first input is equal to the second input when
+ * you are able to modify the first string input. The following modifications to the first string input
+ * are permitted: letters may be capitalized, or discarded, but otherwise not changed.  
+ * 
+ * examples:  
+ * ('ebCde','BCD') TRUE  
+ * ('eCB','ECB') TRUE (e was uppercased)  
+ * ('BcdEF','BCEF') TRUE (c was uppercased, d was discarded)  
+ * ('BCD','BcD') FALSE (C could not be lowercased)  
+ * @param {string} string1 
+ * @param {string} string2 
+ * @returns string "TRUE" or "FALSE"
+ */
 
 function match(string1, string2) {
-
     // check if both string are of the same
     if(string1 === string2) return "TRUE";
-
     // check if string1 has less characters than the string2
     if(string1.length < string2.length) return "FALSE";
 
-
+    // exclude the extra characters from string1 if any.
     var regex = ".?";
     var regexGroup = "";
     string2.split('').forEach((letter, index) => {
         regex += `(${letter}).?`;
         regexGroup += `$${index+1}`;
-        console.log(letter);
     });
     
     const newString1 = string1.replace(new RegExp(regex, "gi"), regexGroup);
-    
-    console.log(regex);
-    console.log(regexGroup);
-    console.log(newString1);
-
+    // check if newString1 not equals to the string2
     if(newString1.toLowerCase() !==  string2.toLowerCase()) return "FALSE";
 
-
-    var newString2 = "";
-    // string1.split('').forEach((letter, index) => {
-
-    //     console.log(letter, string2, index);
-    //     console.log(string2[index]);
-
-    //     if(letter === string2[index]) {
-    //         newString2 += letter;
-    //         return;
-    //     }
-    //     else if(letter === string2[index].toUpperCase()) {
-    //         newString2 += letter;
-    //         return;
-    //     }
-
-    // });
-
-    console.log(newString1, string2);
     var newString2 = "";
     for (let i = 0; i < newString1.length; i++) {
-        //console.log(string1[i]);
         if (newString1[i] === string2[i]) {
             newString2 += string2[i];
         }
@@ -69,10 +44,7 @@ function match(string1, string2) {
         }
     }
 
-    console.log(newString2);
-
     if(newString2 !== string2) return "FALSE";
-
 
     return "TRUE";
 }
